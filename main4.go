59e131282ee4bab5d32227cec0d759a561b0f2fb
郑阳康 <2126483838@qq.com>
2025-02-19 18:40:03 +0800

    接口的实例,就是针对多个接口体方法的封装

--- main4.go
+++ main4.go
@@ -1,6 +1,50 @@
 package main
 
+import (
+	//"bufio"
+	"fmt"
+	"math"
+)
+
+type rect struct {
+	w int
+	h int
+}
+type circle struct {
+	radius int
+}
+type geometry interface {
+	area() float64
+	perimeter() float64
+}
+
+func newr(w, h int) rect {
+	return rect{w: w, h: h}
+}
+func newc(r int) circle {
+	return circle{radius: r}
+}
+func (r rect) area() float64 {
+	return float64(r.h * r.w)
+}
+func (r rect) perimeter() float64 {
+	return 2 * float64(r.w+r.h)
+}
+func (c circle) area() float64 {
+	return math.Pi * float64(c.radius*c.radius)
+}
+func (c circle) perimeter() float64 {
+	return 2 * math.Pi * float64(c.radius*c.radius)
+}
+
 // 测试接口专用的文件
 func main() {
+	r := newr(4, 3)
+	c := newc(4)
+	fmt.Println(two(r))
+	fmt.Println(two(c))
+}
 
+func two(s geometry) (float64, float64) {
+	return s.area(), s.perimeter()
 }
