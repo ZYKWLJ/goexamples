0ea96d1ade4e62f940edca3c1c36c6b65b03ddbd
郑阳康 <2126483838@qq.com>
2025-02-19 19:26:55 +0800

    泛型链表的实现

--- main5.go
+++ main5.go
@@ -5,13 +5,13 @@ import (
 	"math"
 )
 
-type listNode struct {
-	next *listNode
-	val  float64
+type listNode[K comparable] struct {
+	next *listNode[K]
+	val  K
 }
 
-func newList(val float64) *listNode {
-	return &listNode{
+func newList[K comparable](val K) *listNode[K] {
+	return &listNode[K]{
 		next: nil,
 		val:  val,
 	}
@@ -19,12 +19,20 @@ func newList(val float64) *listNode {
 
 func main() {
 	fmt.Println("演示泛型的使用", math.Pi)
-	fmt.Println("下面是一个链表")
+	fmt.Println("下面是一个int类型链表")
 	head := newList(math.Pi)
 	head.next = newList(math.Pi * math.Pi)
 	head.next.next = newList(math.Pi * math.Pi * math.Pi)
 	for head != nil {
 		fmt.Println(head.val)
 		head = head.next
+	}
+	fmt.Println("下面是一个string类型链表")
+	head1 := newList("你好呀")
+	head1.next = newList("我是")
+	head1.next.next = newList("郑阳康")
+	for head1 != nil {
+		fmt.Println(head1.val)
+		head1 = head1.next
 	}
 }
