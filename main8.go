970ce88e823707b178e4678efd1e3cb1d7ff3f20
郑阳康 <2126483838@qq.com>
2025-02-20 12:03:02 +0800

    有缓冲通道的定义，可以缓冲内容

--- main8.go
+++ main8.go
@@ -10,14 +10,18 @@ type SYNACK struct {
 // 通道：语言级别的消息队列，是go语言具有极简的线程通信方式——基于消息队列通信方式
 func main() {
 	fmt.Println("创建一个通道")
-	message := make(chan SYNACK)
+	message := make(chan SYNACK, 10)
 	fmt.Println("将信息传入通道里面")
 	synack := SYNACK{SYN: "hey, this is my message, have you received it?", ACK: "Yes, I received it, and i will accept it ,thanks"}
 	go func() { //这里是阻塞的发送和接受
-		fmt.Println(synack.SYN)
-		message <- synack
+		for i := 0; i < 10; i++ {
+			message <- synack
+		}
 	}() //这是一个匿名函数
 
 	ACK := <-message
-	fmt.Println(ACK.ACK)
+	for i := 0; i < 10; i++ {
+		fmt.Println(synack.SYN, " times= ", i)
+		fmt.Println(ACK.ACK, " times= ", i)
+	}
 }
